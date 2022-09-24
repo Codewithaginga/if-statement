82ce623915c4fde822ee4db63de47cf50d68e2f0
aginga <brianaginga35@gmail.com>
2022-09-24 12:24:59 +0300

    new code

--- trufls.py
+++ trufls.py
@@ -1,1 +1,15 @@
+def lang(a, b):
 
+    for la in a:
+
+        if la == b:
+
+            return True
+    return False
+
+
+languages = ['python', 'kotlin', 'javascript']
+
+lag = lang(languages, 'python')
+
+print(lag)
